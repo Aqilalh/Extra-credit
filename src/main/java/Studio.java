d06cb9dfd10ef5b56e4c63008f8352e7e4139e9a
Aqilalh <Aqilalhaidari@gmail.com>
2022-09-05 20:50:57 -0400

    Finished work

---
+++ src/main/java/Studio.java
@@ -0,0 +1,17 @@
+public class Studio {
+    public static void main(String[] args) {
+
+        String[] beyonceSongs = {"Who runs the world", "Halo", "Thick"};
+        Artist artist1 = new Artist("Beyonce", "Universal", beyonceSongs);
+        artist1.playPlayList(); // playing somngs by artist 1
+
+        System.out.println("====================================");
+
+        String[] poloGSongs = {"RapStar", "Block-Party"};
+        Artist artist2 = new Artist("PoloG", "Interscope", poloGSongs);
+        artist2.playPlayList(); //playing songs by artist2
+
+
+    }
+}
+
